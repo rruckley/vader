9d82d514c4a264c43334891eef66f1de07d28896
Ryan Ruckley <ryan.ruckley@optus.com.au>
2023-01-20 15:12:10 +1100

    initial rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+
+use rocket::http::ContentType;
+use rocket::fs::{FileServer,relative};
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+async fn index() -> (ContentType, &'static str) {
+    (ContentType::HTML, "
+    ")
+}
+
+#[post("/grade")]
+async fn grade() -> (ContentType, &'static str) {
+    (ContentType::HTML, "
+    ")
+}
+
+#[launch]
+async fn rocket() -> _ {
+    rocket::build()
+        .mount("/",routes![index,grade])
+        .mount("/static", FileServer::from(relative!("static")))
 }
